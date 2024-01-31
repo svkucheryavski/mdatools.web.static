89fb66e818e840b0b5d34a473555ac85e9dac82e
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2024-01-31 14:47:22 +0100

    improvements to DD-SIMCA (0.8.1) and adding PWA option to the app

---
+++ ddsimca/service-worker.js
@@ -0,0 +1,25 @@
+// service-worker.js
+const CACHE_NAME = 'ddsimca-cache';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/ddsimca/',
+        '/ddsimca/index.html',
+        '/ddsimca/ddsimca.js',
+        '/ddsimca/ddsimca.wasm',
+        '/ddsimca/logo.svg',
+        '/ddsimca/icon.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
