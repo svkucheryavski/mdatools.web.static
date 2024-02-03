19a3502fa7297d174527eef028b1034daf1c3db9
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2024-02-03 20:58:36 +0100

    added update promise to service worker

--- ddsimca/service-worker.js
+++ ddsimca/service-worker.js
@@ -1,9 +1,10 @@
 // service-worker.js
-const CACHE_NAME = 'ddsimca-cache-0.9.0';
+const CACHE_NAME = 'ddsimca-cache';
+const CACHE_VERSION = '0.9.0';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(`${CACHE_NAME}-${CACHE_VERSION}`).then((cache) => {
       return cache.addAll([
         '/ddsimca/',
         '/ddsimca/index.html',
@@ -20,6 +21,22 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// update to new version when necessary
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName.startsWith(CACHE_NAME) && cacheName !== `${CACHE_NAME}-${CACHE_VERSION}`) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      );
     })
   );
 });
