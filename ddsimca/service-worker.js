98ecfc867f952136a5dc696d3e208fa4f44b97a3
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2024-03-21 08:57:49 +0100

    tuning service worker

--- ddsimca/service-worker.js
+++ ddsimca/service-worker.js
@@ -1,12 +1,13 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-cache';
-const CACHE_VERSION = '0.9.1';
+const CACHE_VERSION = '0.9.1a';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(`${CACHE_NAME}-${CACHE_VERSION}`).then((cache) => {
       return cache.addAll([
         '/ddsimca/',
+        '/ddsimca/manifest.json',
         '/ddsimca/index.html',
         '/ddsimca/ddsimca.js',
         '/ddsimca/ddsimca.wasm',
@@ -14,15 +15,19 @@ self.addEventListener('install', (event) => {
         '/ddsimca/icon.png',
       ]);
     })
-  );
+    .then(() => console.log('Resources cached successfully.'))
+    .catch(error => console.error('Error caching resources:', error))
+   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+   console.log("ddsimca: try to fetch " + event.request);
+   event.respondWith(
+      caches.match(event.request).then((response) => {
+         console.log("1: " + response)
+         return response || fetch(event.request);
+      })
+   );
 });
 
 // update to new version when necessary
@@ -32,6 +37,7 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName.startsWith(CACHE_NAME) && cacheName !== `${CACHE_NAME}-${CACHE_VERSION}`) {
+            console.log("deleting cache")
             return caches.delete(cacheName);
           }
           return null;
