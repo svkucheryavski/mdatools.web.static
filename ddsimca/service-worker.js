3c87016c8188a631564a026c893e026577ac26b6
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2024-03-21 15:33:54 +0100

    another try to fix cache

--- ddsimca/service-worker.js
+++ ddsimca/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-cache';
-const CACHE_VERSION = '0.9.1g';
+const CACHE_VERSION = '0.9.1h';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
