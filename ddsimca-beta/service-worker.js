b08261f3b102fceba51f691981c0bb4f7daf40b2
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2024-08-19 12:09:54 +0200

    fixed typo in path to wasm file

--- ddsimca-beta/service-worker.js
+++ ddsimca-beta/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-beta-cache';
-const CACHE_VERSION = '2.0.0b';
+const CACHE_VERSION = '2.0.0c';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
