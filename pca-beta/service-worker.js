f5f8e4f2aeae0c2b60060e03042124948d711f7c
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2024-10-14 15:18:44 +0200

    new build for pca app

--- pca-beta/service-worker.js
+++ pca-beta/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'pca-beta-cache';
-const CACHE_VERSION = '0.0.3c';
+const CACHE_VERSION = '0.0.3d';
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(`${CACHE_NAME}-${CACHE_VERSION}`).then((cache) => {
