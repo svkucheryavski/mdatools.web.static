6611815be6c32fb35dd500b8c73bb62363a42af2
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2025-09-08 10:42:58 +0200

    new version of ddsimca-love

--- ddsimca-love/service-worker.js
+++ ddsimca-love/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-cache';
-const CACHE_VERSION = '1.2.1a';
+const CACHE_VERSION = '1.2.2a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
