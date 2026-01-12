33e0bbc31b8eb53692eca899afe80b3341994217
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2026-01-12 12:17:10 +0100

    new version of pca

--- pca/service-worker.js
+++ pca/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'pca-cache';
-const CACHE_VERSION = '2.1.5a';
+const CACHE_VERSION = '2.1.7a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
