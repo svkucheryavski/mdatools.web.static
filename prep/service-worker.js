d1f56ca1a7b1cc61228147bef6b4bd7aa76f0801
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2026-02-16 17:06:15 +0100

    new version of prep app

--- prep/service-worker.js
+++ prep/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'prep-cache';
-const CACHE_VERSION = '1.2.2a';
+const CACHE_VERSION = '1.2.3a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
