fcf4fa6b2e0657a81d29a5d0258a0dc90aeca114
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2026-03-29 18:00:49 +0200

    added new app - pls1da

---
+++ pls1da/service-worker.js
@@ -0,0 +1,50 @@
+// service-worker.js
+const CACHE_NAME = 'pls1da-cache';
+const CACHE_VERSION = '1.5.0a';
+self.addEventListener('install', (event) => {
+   self.skipWaiting();
+   event.waitUntil(
+    caches.open(`${CACHE_NAME}-${CACHE_VERSION}`).then((cache) => {
+      return cache.addAll([
+        '/pls1da/',
+        '/pls1da/manifest.json',
+        '/pls1da/index.html',
+        '/pls1da/pls1da.js',
+        '/pls1da/pls1da.wasm',
+        '/pls1da/logo.svg',
+        '/pls1da/icon.png',
+      ]);
+    })
+    .then(() => console.log('Resources cached successfully.'))
+    .catch(error => console.error('Error caching resources:', error))
+   );
+});
+
+self.addEventListener('fetch', (event) => {
+   // console.log("pca: try to fetch:")
+   // console.log(event.request);
+   event.respondWith(
+      caches.match(event.request, {ignoreSearch: true, ignoreMethod: true, ignoreVary: true}).then((response) => {
+         // console.log("cache response: ");
+         // console.log(response);
+         return response || fetch(event.request);
+      })
+   );
+});
+
+// update to new version when necessary
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+         cacheNames.map((cacheName) => {
+            if (cacheName.startsWith(CACHE_NAME) && cacheName !== `${CACHE_NAME}-${CACHE_VERSION}`) {
+               console.log("deleting cache")
+               return caches.delete(cacheName);
+            }
+            return null;
+        })
+      );
+    }).then(() => self.clients.claim()) // this is needed to start controling clients immediately
+  );
+});
